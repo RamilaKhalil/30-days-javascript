c2bcc720badc89daf61221858d48d53e573e515c
Ramila Khalil <ramilamirker00@gmail.com>
2024-11-19 20:11:33 +0500

    day2

---
+++ datatype.2/inex.js
@@ -0,0 +1,86 @@
+// let a = "ali";
+// let greeting = ` hello ${a}`;
+
+// // @ts-ignore
+// console.log(greeting); // Output: hello ali
+
+
+// let d = "ali";
+// let e = 'ali';
+// let f = `ali`;
+// console.log(d);
+// console.log(e);
+// console.log(f); 
+
+
+// let a = `jjdjsncjjskmsm
+// sncksncksnck`;
+
+// let b = "jcifjeihf
+// sjdijfijf" // error
+
+// let c = "jhgghjnb\njnjj"
+// console.log(c);
+
+// let js = "Javascript";
+// console.log(js.startsWith("Java")); // boolean return
+
+// let js = "Javascript";
+
+// console.log(js.endsWith("script")); 
+
+// console.log("Hello World".includes("World"));
+
+
+//console.log("Hello World".includes("World")); // true
+
+// let a = "ayesha "
+// console.log(a.repeat(3));
+
+// let a = "ayesha";
+// console.log(a.padStart(12 ,"bilal"));
+
+// let name = "Ali";
+// console.log(name.padEnd(10, "bilal")); 
+
+
+// let char = "MujheJavaScriptseekhnihai";
+// console.log(char.split(" ")); 
+// ["MujheJavaScriptseekhnihai"]
+
+
+// let char = "MujheJavaScriptseekhnihai".split("");
+// console.log(char);
+// ['M', 'u', 'j', 'h', 'e', 'J',
+//   'a', 'v', 'a', 'S', 'c', 'r',
+//   'i', 'p', 't', 's', 'e', 'e',
+//   'k', 'h', 'n', 'i', 'h', 'a',
+//   'i'
+// ]
+
+// let data = "Apple , Banana ,  Cherry";
+// let fruits = data.split(",");
+// console.log(fruits);
+
+// let text = "Ali,Ahmed,Asim,Amna";
+// let limited = text.split(",", 3); // split at comma and take only 3 elements
+// console.log(limited);
+
+// let name = "Ramila";
+// let characters = name.split( ); // full ramila
+// console.log(characters);
+
+// let name = "Ramila";
+// let characters = name.split(""); // [ 'R', 'a', 'm', 'i', 'l', 'a' ]
+// console.log(characters);
+
+
+// let str = "123";
+// let num = Number(str);
+// console.log(num); // Output: 123 (as a number)
+
+// let invalidStr = "123abc";
+// console.log(Number(invalidStr)); // Output: NaN
+
+
+
